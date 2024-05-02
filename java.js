f949558c705b43c9f25fd222af377df3057a210b
Shavar2743 <markus.shavar@web.de>
2024-05-02 20:30:32 +0200

    liste erstellt

---
+++ java.js
@@ -0,0 +1,14 @@
+let temp = 20
+
+let fahrenheit = temp * 1.8 + 32;
+
+document.write(fahrenheit)
+
+let liste = []
+
+for (let i = 0; i < 5; i++) {
+    let eingabe = prompt("Erweitere die Liste:");
+    liste.push(eingabe);
+}
+
+console.log(liste)
